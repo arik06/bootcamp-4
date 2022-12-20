a534ed764dba41dd6778690c50ab7267ae57cfc9
arik06 <pepeortiz1125@gmail.com>
2022-12-19 23:00:15 -0600

    segundo commit

---
+++ my-app/src/assets/fake-data/slider.js
@@ -0,0 +1,24 @@
+import sliderImg01 from "../images/slider1.png";
+import sliderImg02 from "../images/slider.png";
+import sliderImg03 from "../images/slider.png";
+
+export default sliderData = [
+    {
+        id:"01",
+        title: "Tenemos la comida mas rica y divertida para ti ",
+        desc: "lorem ipsum dolor sit amet, consectetur",
+        imgUrl:sliderImg01,
+    },
+    {
+        id:"02",
+        title: "Creemos en la diversion al comer",
+        desc: "lorem ipsum dolor sit amet, consect",
+        imgUrl:sliderImg02,
+    },
+    {
+        id:"03",
+        title: "Conoce, come y disfruta la verdadera comida",
+        desc: "lorem ipsum dolor sit amet, consect",
+        imgUrl:sliderImg03,
+    },
+];
